99396a12d3a0101992ac40b8d97196f7925f16c2
fenira that't it <108204426+ambufenn@users.noreply.github.com>
2025-11-10 23:13:45 +0700

    Create model.py

---
+++ model.py
@@ -0,0 +1,7 @@
+from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
+
+def load_model():
+    model_name = "facebook/blenderbot-400M-distill"
+    tokenizer = BlenderbotTokenizer.from_pretrained(model_name)
+    model = BlenderbotForConditionalGeneration.from_pretrained(model_name)
+    return tokenizer, model
