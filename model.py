30e9c86efec0a43f746949bac868b60dd458f6ea
fenira that't it <108204426+ambufenn@users.noreply.github.com>
2025-11-11 23:47:22 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,15 @@
+# model.py
 import os
 from huggingface_hub import login
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def load_model():
     login(token=os.environ["HUGGINGFACE_TOKEN"])
-    tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
+    model_name = "meta-llama/Llama-3.2-3B-Instruct"
+    
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(
-        "google/gemma-2b-it",
+        model_name,
         device_map="cpu",
         torch_dtype="auto",
         low_cpu_mem_usage=True
